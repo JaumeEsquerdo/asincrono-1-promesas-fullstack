802ec7d43753284927d718204a563159a921e746
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-03-17 00:08:13 +0100

    Update SeccionDinamica.jsx

--- promesas-asincrono/src/components/SeccionDinamica.jsx
+++ promesas-asincrono/src/components/SeccionDinamica.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router";
-import Dnd from "@/pages/Dnd";
-import Rawg from "@/pages/Rawg";
-import Pokemon from "@/pages/Pokemon";
+import Dnd from "@/pages/Dnd.jsx";
+import Rawg from "@/pages/Rawg.jsx";
+import Pokemon from "@/pages/Pokemon.jsx";
 
 const SeccionDinamica = () => {
     const {seccionId} = useParams(); //obtiene el id de la seccion de la url
